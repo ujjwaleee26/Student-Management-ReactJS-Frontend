ce7c4b9a0079afa8eb62a4a6256ffe5ef78ec705
Ujjwal Sinha <sinhaujjwal04@gmail.com>
2024-01-24 09:43:53 +0530

    correctly fetched graphql api on react frontend

--- src/App.tsx
+++ src/App.tsx
@@ -3,48 +3,63 @@ import './App.css';
 import {useQuery,gql} from '@apollo/client';
 
 
-export type Launch={
-        "id": string,
-        "name": string,
-        "contact": string,
-        "result": 
-          {
-            "subjectName": string,
-            "marks": number
-          }
+interface StudentSubject {
+  subjectId: string;
+  subjectName: string;
+  marks: number;
+}
+
+interface Launch {
+  id: string;
+  name: string;
+  contact: string;
+  result: StudentSubject[];
 }
 
 const GET_DATA=gql`{
-  getAllStudents {
-    id
-    name
-    contact
-    result {
-      subjectName
-      marks
+     getAllStudents {
+        id
+        name
+        contact
+        result {
+           subjectId
+           subjectName
+           marks
+        }
     }
   }
-}
+
 `;
 
 function App() {
 
   const{loading,error,data}=useQuery(GET_DATA);
 
-  useEffect(()=>{
-    console.log(loading,error,data)
-  });
+  useEffect(() => {
+    console.log(loading, error, data);
+  }, [loading, error, data]);
 
   return (
     <div className="App">
-      
-     {data?data.getAllStudents.map((launch:Launch)=>{
-      return <p> {launch.id+' '+launch.name+' '+launch.result}</p>
-     }):null}
-
+      {data
+        ? data.getAllStudents.map((launch:Launch) => (
+            <div key={launch.id}>
+              <p>
+                {launch.id} {launch.name} {launch.contact}{' '}
+                {launch.result.map((subject) => (
+                  <span key={subject.subjectId}>
+                    {subject.subjectName}: {subject.marks}{' '}
+                  </span>
+                ))}
+              </p>
+            </div>
+          ))
+        : null}
     </div>
   );
 }
+
+
 
 export default App;
  
